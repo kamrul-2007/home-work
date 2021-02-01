f32de0f61ac54009e15d824fa3ae72833af5bd94
kamrul-2007 <kamrul.kth.islam.1996@gmail.com>
2021-02-01 23:33:28 +0600

    new file

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+		function add() {
+			var a = 10;
+			var b = 20;
+			var result = a + b;
+			document.getElementById('one').innerHTML = result;
+		}
+
+		function multi(a, b) {
+			var result = a * b;
+			document.getElementById('tow').innerHTML = result;
+		}
+
+		function subt(a, b) {
+			var result = a - b;
+			document.getElementById('three').innerHTML = result;
+		}
+
+		function divi(a, b) {
+			var result = a / b;
+			document.getElementById('four').innerHTML = result;
+		}
+
+		function modu(a, b) {
+			var result = a % b;
+			document.getElementById('five').innerHTML = result;
+		}
